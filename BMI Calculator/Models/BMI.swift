c8901cc5a32922b7e5907af3d58470ebf0e17641
Feng Wei <fwei95@uw.edu>
2020-04-27 16:05:10 -0700

    BMI calculator done

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by fengwei on 4/27/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
